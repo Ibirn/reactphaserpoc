b4d94fa41c413f30ceff50896b9f861f97801e67
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-10-25 15:42:57 -0600

    add button counter in App.js to test the effects of state change with phaser. The button does not mess up the phaser map, however, state is not persistent when we route from the map to a scene and back. The button resets annd state is not saved. Could be problematic in the app if we want to save state on our game frame.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import Scene1 from './components/Scene1';
-import Scene2 from './components/Scene2';
-import Scene3 from './components/Scene3';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'
+import React, { useState } from "react";
+import Scene1 from "./components/Scene1";
+import Scene2 from "./components/Scene2";
+import Scene3 from "./components/Scene3";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-
+  //checking to see if updating state impacts the phaser map at all
+  //this does not affect the phaser map, but every time we navigate away, it does re-render the button count, which would not be ideal in the app because we want to have persistent state in our nav/game frame 
+  const [counter, setCounter] = useState(0);
   return (
     <BrowserRouter>
       <div className="App">
+        <button onClick={() => setCounter((prev) => prev + 1)}>
+          {counter}
+        </button>
         <Navbar />
-          <Route path='/scene1' component={Scene1} />
-          <Route path='/scene2' component={Scene2} />
-          <Route path='/scene3' component={Scene3} />
+        <Route path="/scene1" component={Scene1} />
+        <Route path="/scene2" component={Scene2} />
+        <Route path="/scene3" component={Scene3} />
       </div>
     </BrowserRouter>
   );
